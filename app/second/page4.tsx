4176e5af2e3abc282bcdbd949039415b0920cfc2
Satish Kumar Goud <askgoud103-blip@gmail.com>
2025-12-05 16:42:54 +0530

    Remove center image, mobile navbar fix

--- app/second/page4.tsx
+++ app/second/page4.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function HomePage() {
   const thumbnails = [

3588a9ce3e9762e07db23941921ecb96bd2f3a8e
Satish Kumar Goud <askgoud103-blip@gmail.com>
2026-02-11 15:30:04 +0530

    Show Contact or Visit when price is missing

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,10 +10,13 @@ export const normalize = (value: string) => {
     .replace(/-+/g, "-");      // Double hyphens to single
 };
 
-export const formatPrice = (price: number) => {
+export const formatPrice = (price?: number) => {
+  if (!price) return "Contact or Visit";
+
   return price.toLocaleString("en-IN", {
     style: "currency",
     currency: "INR",
     maximumFractionDigits: 0,
   });
 };
+

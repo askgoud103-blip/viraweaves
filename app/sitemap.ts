f21bf927315031b49905a343e198d9b9ba608083
Satish Kumar Goud <askgoud103-blip@gmail.com>
2026-04-02 16:43:46 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://viraweaves.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://viraweaves.vercel.app/category/all",
+      lastModified: new Date(),
+    }
+  ];
+}

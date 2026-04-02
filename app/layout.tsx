b8f9bbdea25f0e17fbaefe7882bb4acb9aea132d
Satish Kumar Goud <askgoud103-blip@gmail.com>
2026-04-02 16:51:02 +0530

    added brand schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,30 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head>
+     <head>
+
 <meta name="google-site-verification" content="YyAAWKFtqBCoEis49yQH8LqWj6qSCj0tWAhWysz0unU" />
 
 <title>Vira Weaves | Premium Bridal Sarees</title>
 
 <meta name="description" content="Premium Bridal & Designer Sarees curated for grand occasions across India." />
+
+<script
+type="application/ld+json"
+dangerouslySetInnerHTML={{
+__html: JSON.stringify({
+"@context": "https://schema.org",
+"@type": "Organization",
+"name": "Vira Weaves",
+"url": "https://viraweaves.vercel.app",
+"sameAs": [
+"https://www.instagram.com/vira_weaves_/",
+"https://www.facebook.com/profile.php?id=61570486516513"
+]
+})
+}}
+/>
+
 </head>
 
       <body className="bg-black text-white">
